1aa5e4d31207cf9cb306ea0c175fc48855dea478
Anthony T. <antonis301@gmail.com>
2024-06-25 19:32:16 +0300

    Added menu to Main program

--- src/main/java/com/sorting/Main.java
+++ src/main/java/com/sorting/Main.java
@@ -1,6 +1,20 @@
 package com.sorting;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+
 public class Main {
+    // This is designed in a neat way, whenever a new Sorting algorithm is added
+    // A demo function using its class is made, and then it can be easily added to the menu
+    // by adding its demo function name and its name.
+
+    // will add more sorting algorithms in the future
+    static String[] methodNames = {"demoBubbleSort", "demoSelectionSort"};
+    static String[] names = {"Bubble sort", "Selection sort"};
+
+
     public static void printArr(int[] arr) {
         System.out.print("[ ");
         for (int i : arr) {
@@ -33,9 +47,52 @@ public class Main {
         System.out.println("Operations done : " + SelectionSort.operations);
     }
 
+    public static void displayMenu() {
+        System.out.println("Welcome to Sorting Algorithms with Java");
+        System.out.println("A collection of sorting algorithms by Anthony T.");
+        System.out.println("MENU");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println((i+1) + " - " + names[i]);
+        }
+        System.out.println("Enter # of choice : ");
+    }
+
+    public static int getUserInput() {
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String input = stdin.readLine();
+            int selection = Integer.parseInt(input);
+
+            if (selection > names.length || selection < 1) {
+                throw new NumberFormatException();
+            }
+
+            return selection;
+        } catch (IOException e) {
+            System.err.println("Cannot read user input, exiting.");
+            System.exit(1);
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid input, exiting.");
+            System.exit(1);
+        }
+        
+        return -1;
+    }
+
+    public static void invokeSorting(int choice, int arr[]) {
+        try {
+            Method method = Main.class.getMethod(methodNames[choice-1], int[].class);
+            method.invoke(null, arr);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
     public static void main(String[] args) {
         int arr[] = {1,4,5,3,2,6,9,8,7};
 
-        demoSelectionSort(arr);
+        displayMenu();
+        int choice = getUserInput();
+        invokeSorting(choice, arr);
     }
 }
